778d01a844cf64257c6859e07fcab2066e2901a7
noelochoa <35244256+noelochoa@users.noreply.github.com>
2020-03-14 01:49:54 +0900

    Fix search input format

---
+++ src/pages/accounts/Edit.vue
@@ -0,0 +1,17 @@
+<template>
+    <q-page class="flex flex-center">
+        <h3>Edit</h3>
+    </q-page>
+</template>
+<style lang="scss" scoped></style>
+<script>
+export default {
+    name: "CustomerEdit",
+    meta() {
+        return {
+            title: "Customer Details"
+        };
+    },
+    created() {}
+};
+</script>
